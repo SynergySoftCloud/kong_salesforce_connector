741a7d332b45ece4534079f8327f880943076b22
Fylyp Berzenin <fylyp.berzenin@synergysoft.ch>
2020-03-31 15:43:08 +0300

    salesforce connector

---
+++ src/test/java/ch/synergysoft/siebel/SiebelApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.synergysoft.siebel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiebelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
